ce1b5be2c5eaffc982646d0201957ed6dd42cca6
Dcrony <ibrahimabdulmajeed14@gmail.com>
2025-10-20 20:13:10 +0100

    Fix case-sensitive import for LandingPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
-// import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import CreateEvent from "./pages/CreateEvent";
 import MyTickets from "./pages/MyTickets";
 import Dashboard from "./pages/Dashboard";
-// import EditEvent from "./pages/EditEvent";
+import EditEvent from "./pages/EditEvent";
 import NavBar from "./components/NavBar";
 import Success from "./pages/Success";
 import StatsDashboard from "./pages/StatsDashboard";
@@ -35,16 +35,16 @@ function Layout() {
       {!hideNavAndSidebar && <Sidebar />}
       
       <Routes>
-        {/* <Route path="/login" element={<LandingPage />} /> */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/events" element={<Home />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/create" element={<CreateEvent />} />
         <Route path="/my-tickets" element={<MyTickets />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin/dashboard" element={<StatsDashboard />} />
         <Route path="/Eventdetail/:eventId" element={<EventDetail />} />
-        {/* <Route path="/edit/:eventId" element={<EditEvent />} /> */}
+        <Route path="/edit/:eventId" element={<EditEvent />} />
         <Route path="/success" element={<Success />} />
         <Route path="/live/events" element={<LiveEvent />} />
         <Route path="/settings" element={<Settings />} />
